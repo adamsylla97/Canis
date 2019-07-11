8cbc2ed0fc257221b25bafdf172d13804a70cb70
adamsyll <adam.sylla@idemia.com>
2019-07-11 22:22:32 +0200

    *added workers adapter for recycle view

--- app/src/main/java/com/example/canis/WorkersInformationModule/WorkersAdapter.kt
+++ app/src/main/java/com/example/canis/WorkersInformationModule/WorkersAdapter.kt
@@ -1,4 +1,27 @@
 package com.example.canis.WorkersInformationModule
 
-class WorkersAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.canis.WorkersInformationModule.communication.Worker
+
+class WorkersAdapter(private val listener: (Long) -> Unit): RecyclerView.Adapter<WorkersAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WorkersAdapter.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: WorkersAdapter.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHolder(private val view: View, private val itemClick: (Long)->Unit):RecyclerView.ViewHolder(view){
+        fun bind(worker: Worker){
+            //TODO implement bind method for working that binds every card to another fragment
+        }
+    }
+
 }
