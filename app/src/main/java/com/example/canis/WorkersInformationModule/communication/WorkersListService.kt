3cc3bbb18cfa979f8b061f2f059725e38afe6eb4
adamsyll <adam.sylla@idemia.com>
2019-07-11 21:31:57 +0200

    added interface for fetching list of workers

--- app/src/main/java/com/example/canis/WorkersInformationModule/communication/WorkersListService.kt
+++ app/src/main/java/com/example/canis/WorkersInformationModule/communication/WorkersListService.kt
@@ -1,4 +1,11 @@
 package com.example.canis.WorkersInformationModule.communication
 
+import retrofit2.http.GET
+
 interface WorkersListService {
+
+    @GET("/api/workers")
+    suspend fun listOfWorkers(): List<Int>
+
+
 }
