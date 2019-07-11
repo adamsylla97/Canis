f533cb536eb50dbe4499e58bd62058db3d854174
adamsyll <adam.sylla@idemia.com>
2019-07-11 22:17:16 +0200

    *implemented koin and instance provider *added start koin method to CanisApplication class

--- app/src/main/java/com/example/canis/CanisApplication.kt
+++ app/src/main/java/com/example/canis/CanisApplication.kt
@@ -1,13 +1,22 @@
 package com.example.canis
 
 import android.app.Application
+import com.example.canis.WorkersInformationModule.InstanceProvider
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class CanisApplication : Application() {
 
     override fun onCreate(){
 
         super.onCreate()
-        //TODO preparred for start koin method
+
+        startKoin {
+            // Android context
+            androidContext(this@CanisApplication)
+            // modules
+            modules(InstanceProvider.commonModule)
+        }
 
     }
 
--- app/src/main/java/com/example/canis/WorkersInformationModule/InstanceProvider.kt
+++ app/src/main/java/com/example/canis/WorkersInformationModule/InstanceProvider.kt
@@ -1,7 +1,20 @@
 package com.example.canis.WorkersInformationModule
 
+import com.example.canis.WorkersInformationModule.communication.WorkersInformationService
+import com.example.canis.WorkersInformationModule.communication.WorkersListService
+import com.example.canis.WorkersInformationModule.data.RestWorkerProvider
+import com.example.canis.WorkersInformationModule.data.WorkersDataProvider
+import com.example.canis.WorkersInformationModule.model.WorkersService
+import org.koin.dsl.module
+
 object InstanceProvider {
 
-    //`TODO prepared for lated implementation of koin
+    val commonModule = module {
 
+        single { RestServiceBuilder.build(WorkersInformationService::class.java) }
+        single { RestServiceBuilder.build(WorkersListService::class.java)}
+        single<WorkersDataProvider> {RestWorkerProvider(get(), get())}
+        single { WorkersService(get())}
+
+    }
 }
