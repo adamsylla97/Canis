8c0090020f546b9921f7b8944359e8f1421282fd
adamsyll <adam.sylla@idemia.com>
2019-07-11 22:12:51 +0200

    *removed nagivation bar *rewrited main activity to kotlin *changed main fragment to activity *added koin dependencies *updated gradle file with actual libraries for card view and recycle view

--- app/src/main/java/com/example/canis/WorkersInformationModule/data/RestWorkerProvider.kt
+++ app/src/main/java/com/example/canis/WorkersInformationModule/data/RestWorkerProvider.kt
@@ -2,15 +2,17 @@ package com.example.canis.WorkersInformationModule.data
 
 import com.example.canis.WorkersInformationModule.communication.Worker
 import com.example.canis.WorkersInformationModule.communication.WorkersInformationService
+import com.example.canis.WorkersInformationModule.communication.WorkersListService
 
-class RestWorkerProvider(private val restService: WorkersInformationService) : WorkersDataProvider {
+class RestWorkerProvider(private val workerListRestService: WorkersListService,
+                         private val specificWorkerRestService: WorkersInformationService) : WorkersDataProvider {
 
     override suspend fun fetchListOfWorkers(): List<Int> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return workerListRestService.listOfWorkers()
     }
 
     override suspend fun fetchWorker(id: Int): Worker {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return specificWorkerRestService.workerInformation(id)
     }
 
 }
