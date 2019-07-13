99b283e4428389360f79039badbb1491f08afb96
adamsyll <adam.sylla@idemia.com>
2019-07-14 00:01:56 +0200

    *added glide dependency to glide *added worker item layout *added values to dimens for worker_item layout *implemented workers adapter for recycle view

--- app/src/main/java/com/example/canis/WorkersInformationModule/WorkersAdapter.kt
+++ app/src/main/java/com/example/canis/WorkersInformationModule/WorkersAdapter.kt
@@ -1,26 +1,33 @@
 package com.example.canis.WorkersInformationModule
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.canis.R
 import com.example.canis.WorkersInformationModule.communication.Worker
+import kotlinx.android.synthetic.main.worker_item.view.*
 
-class WorkersAdapter(private val listener: (Long) -> Unit): RecyclerView.Adapter<WorkersAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WorkersAdapter.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+class WorkersAdapter(val workersList: List<Worker>, private val listener: (Long) -> Unit): RecyclerView.Adapter<WorkersAdapter.WorkersHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WorkersAdapter.WorkersHolder {
+        val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.worker_item, parent, false)
+        return WorkersHolder(inflatedView, listener)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return workersList.size
     }
 
-    override fun onBindViewHolder(holder: WorkersAdapter.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: WorkersAdapter.WorkersHolder, position: Int) {
+        val item = workersList[position]
+        holder.bind(item)
     }
 
-    class ViewHolder(private val view: View, private val itemClick: (Long)->Unit):RecyclerView.ViewHolder(view){
+    class WorkersHolder(private val view: View, private val itemClick: (Long)->Unit):RecyclerView.ViewHolder(view){
         fun bind(worker: Worker){
-            //TODO implement bind method for working that binds every card to another fragment
+            view.workerName.text = worker.name //TODO add worker for buissness part for process first and last name + inz / mgr / d / prof etc.
+            Glide.with(view).load(worker.photo).into(view.workerImage)
         }
     }
 
