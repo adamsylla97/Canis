f93dab663dd88dee233402e44348bba3691f867d
adamsylla97 <209474@edu.p.lodz.pl>
2019-07-16 21:19:15 +0200

    *added workers fragment *implemented fragment backend for workers module

---
+++ app/src/main/java/com/example/canis/WorkersInformationModule/WorkersFragment.kt
@@ -0,0 +1,55 @@
+package com.example.canis.WorkersInformationModule
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.canis.R
+import com.example.canis.WorkersInformationModule.communication.Worker
+import java.lang.RuntimeException
+
+class WorkersFragment : Fragment() {
+
+    private val onWorkerClickedListener: (Long) -> Unit = {id -> listener?.onWorkerClicked(id)}
+    private var listener: OnWorkerClickedListener? = null
+    private val workersList: List<Worker>? = null
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.worker_module_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.workersRecycleView)
+        val adapter = WorkersAdapter(workersList!!,onWorkerClickedListener)
+        recyclerView.adapter = adapter
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if(context is OnWorkerClickedListener){
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnCourseClickedListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnWorkerClickedListener{
+        fun onWorkerClicked(id: Long)
+    }
+
+    companion object{
+        @JvmStatic
+        fun newInstance() = WorkersFragment()
+    }
+
+}
--- app/src/main/java/com/example/canis/WorkersInformationModule/WorkersMainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.canis.WorkersInformationModule
-
-class WorkersMainActivity {
-
-
-
-}
